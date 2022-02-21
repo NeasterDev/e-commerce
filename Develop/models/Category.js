eb13cba0a2e75bb70fa9bb0f11b9baca925b6e25
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-20 17:05:36 -0800

    added category_name to categroy model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,13 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
   },
   {
     sequelize,
