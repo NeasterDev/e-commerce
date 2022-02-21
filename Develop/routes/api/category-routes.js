169e10c7a987311a9de69d39e3fd72aad0cd830f
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-20 17:39:23 -0800

    added all initial crud routes for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,18 +14,55 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ error: "No category found with that id"});
+      return;
+    }
+    res.json(categoryData);
+  })
+ 
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(newCategory => res.json(newCategory));
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(updatedCategory => res.json(updatedCategory))
+    .catch(err => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(deletedCategory => {
+    if (!deletedCategory) {
+      res.status(404).json({ error: "No category found with that id"});
+      return;
+    }
+    res.json(deletedCategory);
+
+  })
 });
 
 module.exports = router;
