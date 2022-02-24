db26d6b96ff9fd4807ab8633a06289a1bcc1966d
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-24 13:22:28 -0800

    finsihed

--- models/index.js
+++ models/index.js
@@ -6,27 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id'
-})
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   as: 'tag_info',
   through: ProductTag,
   foreignKey: 'product_id'
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   as: 'tag_info',
   through: ProductTag,
   foreignKey: 'tag_id'
-})
+});
+
 module.exports = {
   Product,
   Category,
